293bde127a73cdf02082c4487df334048347f357
LZRhhh <liuzhuoran8@gmail.com>
2020-05-11 18:19:31 -0400

    modified HistoricalMapper

--- src/main/java/com/ece568/stockpro/mapper/HistoricalMapper.java
+++ src/main/java/com/ece568/stockpro/mapper/HistoricalMapper.java
@@ -15,7 +15,7 @@ public interface HistoricalMapper {
     @Select("select * from `historical_data` where symbol = #{symbol}")
     List<HistoricalData> findBySymbol(String symbol);
 
-    @Select("select * from `historical_data` where symbol = #{symbol} limit #{num}")
+    @Select("select * from `historical_data` where symbol = #{symbol} order by date desc limit #{num}")
     List<HistoricalData> getBySymbol(String symbol, int num);
 
     @Insert("insert into historical_data (symbol, close, date, high, low, open, volume)" +
